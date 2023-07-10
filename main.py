de36aca494664a6988962cd4015c1d76fd99dff9
DeerEdge <vislawath.21@gmail.com>
2023-07-09 20:33:56 -0600

    login

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 Window.size = (360, 640)
 
+class LoginWindow(Screen):
+    pass
+
+
+class MainWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
 class MainApp(MDApp):
     def build(self):
-        return Builder.load_file('layout.kv')
+        self.root_widget = Builder.load_file('layout.kv')
+        return self.root_widget
+
+    def login(self, dt):
+        self.root_widget.current = 'main'
 
 MainApp().run()
