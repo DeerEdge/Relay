9f768d623cbfe1cc3ba52f4bceb8f47422051b77
thomaslu678 <thomaslu678@gmail.com>
2023-07-10 23:54:41 -0600

    rename to Relay

--- django/Relay/asgi.py
+++ django/Relay/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TodoAPI project.
+ASGI config for Relay project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TodoAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Relay.settings')
 
 application = get_asgi_application()
