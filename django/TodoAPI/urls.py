a7a62575ea6d0d2551f35587612f727f09e66519
thomaslu678 <thomaslu678@gmail.com>
2023-07-10 23:14:02 -0600

    refactor dir, db only

--- django/TodoAPI/urls.py
+++ django/TodoAPI/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tasks.urls')),
+    path('', include('phone_numbers.urls')),
 
 ]
