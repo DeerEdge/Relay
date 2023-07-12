1d04caa9e0bfff6196a146618e38ecb9c0e74240
thomaslu678 <thomaslu678@gmail.com>
2023-07-11 21:45:18 -0600

    adding fields via migrations

--- django/phone_numbers/models.py
+++ django/phone_numbers/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class PhoneNumber(models.Model):
     phone_number = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    date_joined = models.DateTimeField(null=True)
+    int_phone = models.IntegerField(null=True)
     def __str__(self):
-        return self.phone_number
+        return {self.phone_number, self.name, self.date_joined, self.int_phone}
 
